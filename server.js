539f339b291563d1c557a58c29a1697ae48c00b3
Anthony Clayton <114684904+AnthonyClayton12@users.noreply.github.com>
2025-02-22 11:11:06 -0500

    Update server.js

--- server.js
+++ server.js
@@ -65,7 +65,8 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    passReqToCallback: true
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
